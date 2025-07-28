3268f5b8b177e057b8a3277ae94981c35758e1d3
hx <im@bgzo.cc>
2025-07-28 21:44:36 +0800

    feat: read from html history

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # 让我们快乐地点格子
 
+## 历史遗留
+
+1. [ ] 多个账号的问题，需要从对应账号里面拉数据；
+2. [x] 下载了历史备份，需要这次一次性上传上去
 
 ## 参考项目
 
 - https://github.com/LCMs-YoRHa/From-Bangumi-to-Obsidian
-
+- https://github.com/BGmi/BGmi
 
 ## License
 
--- main.py
+++ main.py
@@ -6,20 +6,30 @@
 @Links : https://github.com/bGZo
 """
 import os
+import re
+
 from bs4 import BeautifulSoup
 
 
 def get_all_bgm_id_from_html_files(directory: str) -> set:
+    pattern = re.compile(r'/subject/(\d+)$')
+    result = set()
     for filename in os.listdir(directory):
         if filename.endswith('.html'):
             filepath = os.path.join(directory, filename)
             with open(filepath, 'r', encoding='utf-8') as f:
                 soup = BeautifulSoup(f, 'html.parser')
                 h3_tags = soup.find_all('h3')
-                print(f'文件: {filename}')
-                for tag in h3_tags:
-                    print(tag.get_text(strip=True))
-    return set()
+                for h3 in h3_tags:
+                    a_tag = h3.find('a', href=True)
+                    if a_tag:
+                        match = pattern.search(a_tag['href'])
+                        if match:
+                            result.add(match.group(1))
+                            print(f'文件: {filename}, ID: {match.group(1)}')
+    return result
 
 if __name__ == '__main__':
-    get_all_bgm_id_from_html_files('./history')
+    target_set = get_all_bgm_id_from_html_files('./history')
+    print(len(target_set))
+    print(target_set)
