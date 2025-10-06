8c7d1a103b9bd2906d01b250ece9eff0806f79aa
hx <im@bgzo.cc>
2025-10-06 18:18:10 +0800

    feat: enhance build for github pages

--- npm-build-and-preview-github-pages.sh
+++ npm-build-and-preview-github-pages.sh
@@ -1,4 +1,7 @@
 # !/bin/bash
+owner = bGZo
+repo = github-pages
+
 # 构建并预览 Astro 项目，然后将其部署到 GitHub Pages
 npm run build
 # 构建目录
@@ -12,9 +15,13 @@ git commit -m "Deploy to GitHub Pages"
 # 更名分支
 git branch -M main
 # 设置远程仓库地址(SSH)
-git remote add origin git@github.com:bGZo/github-pages.git
+git remote add origin git@github.com:${owner}/${repo}.git
 # 推送到GitHub Pages分支（通常是gh-pages分支）
 git push origin main --force
 # 返回上级目录
 cd -
+echo "-----------------------------------------------------"
 echo "Deployed to GitHub Pages successfully."
+echo "Github Pages URL: https://${owner}.github.io/${repo}/"
+echo "Github Repo URL: https://github.com/${owner}/${repo}"
+echo "-----------------------------------------------------"
