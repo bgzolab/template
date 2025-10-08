1f495f7821902d0a5d4914526f6f292b9a012115
hx <im@bgzo.cc>
2025-10-08 20:32:46 +0800

    chore: script to clean up

--- npm-build-and-preview-github-pages.sh
+++ npm-build-and-preview-github-pages.sh
@@ -13,15 +13,16 @@ git add .
 # 提交更改
 git commit -m "Deploy to GitHub Pages"
 # 更名分支
-git branch -M main
+git branch -M gh-pages
 # 设置远程仓库地址(SSH)
 git remote add origin git@github.com:${owner}/${repo}.git
 # 推送到GitHub Pages分支（通常是gh-pages分支）
-git push origin main --force
+git push origin gh-pages --force
 # 返回上级目录
 cd -
-echo "-----------------------------------------------------"
+echo "------------------------------------------------------------------"
 echo "Deployed to GitHub Pages successfully."
 echo "Github Pages URL: https://${owner}.github.io/${repo}/"
-echo "Github Repo URL: https://github.com/${owner}/${repo}"
-echo "-----------------------------------------------------"
+echo "Github Repo URL: https://github.com/${owner}/${repo}/tree/gh-pages"
+echo "------------------------------------------------------------------"
+
