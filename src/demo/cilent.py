2d3775b49f321420c4c1c238defb9f3374984a94
hx <im@bgzo.cc>
2025-12-13 21:21:51 +0800

    feat(demo): add cookie template

--- src/demo/cilent.py
+++ src/demo/cilent.py
@@ -14,15 +14,17 @@ from demo import (api_endpoints)
 class DemoClient:
     def __init__(self):
         self.token = os.getenv("DEMO_TOKEN")
-        # self.cookie = os.getenv("DEMO_COOKIE")
+        self.cookie = os.getenv("DEMO_COOKIE")
 
         if not self.token:
-        # if not self.cookie:
             raise ValueError("DEMO_TOKEN environment variable is not set.")
+        if not self.cookie:
+            raise ValueError("DEMO_COOKIE environment variable is not set.")
+
         self.session = requests.Session()
         self.session.headers.update({
             "Authorization": f"Bearer {self.token}",
-            # "Cookie": f"{self.cookie}",
+            "Cookie": f"{self.cookie}",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:144.0) Gecko/20100101 Firefox/144.0",
             "Referer": "https://www.example.com"
         })
