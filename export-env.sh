81dc484e4a3957c4695c552b9649b933d648d24d
hx <im@bgzo.cc>
2026-01-18 18:20:29 +0800

    feat: add script to export env vars

---
+++ export-env.sh
@@ -0,0 +1,7 @@
+# Source - https://stackoverflow.com/a/30969768
+# Posted by user4040650, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-18, License - CC BY-SA 4.0
+
+set -o allexport
+source ./.env
+set +o allexport
